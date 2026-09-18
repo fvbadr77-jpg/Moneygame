42252802286131ad406db0852291ab262fa4ae95
fvbadr77-jpg <fvbadr77@gmail.com>
2026-09-18 11:37:40 +0300

    Add MoneyGame hardware layer

---
+++ hardware.java
@@ -0,0 +1,6 @@
+public class hardware {
+    public static void main(String[] args) {
+        System.out.println("MoneyGame hardware ready.");
+        System.out.println("Current account: Owner jj");
+    }
+}
